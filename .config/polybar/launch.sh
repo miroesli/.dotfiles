562d373cc0e1c951a7c7f4ec1a5d9496a9d7dbf4
miroesli <misha.roesli@gmail.com>
2019-10-19 11:01:32 -0700

    fixed polybar multi monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,17 +7,17 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Set monitors
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload example &
-#   done
-# else
-#   polybar --reload example &
-# fi
-
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload example &
-done
+  done
+else
+  polybar --reload example &
+fi
+
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+#     MONITOR=$m polybar --reload example &
+# done
 
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you have to change it here too.
