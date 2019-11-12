d68c66befc63d5f1aca98d16e091ad9664ba95b0
miroesli <misha.roesli@gmail.com>
2019-11-11 21:28:16 -0800

    update copyconfig file name and add wal setup

--- configs/copyallconfig.sh
+++ configs/copyallconfig.sh
@@ -4,10 +4,9 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"/among-trees
 # cd "$parent_path"/$1
 
-# TODO: Need to check here if path exists
-
 cp -r .config/ ~
 cp .Xresources ~
 cp .xinitrc ~
 cp .xsettingsd ~
 cp .zshrc ~
+wal -i ../../wallpapers/forestlake.png
