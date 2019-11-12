283bb4619128b6764c5c58b6a6e5d5bbe5cb2f8f
miroesli <misha.roesli@gmail.com>
2019-11-11 21:29:09 -0800

    update stow scripts to use .config only

--- configs/removeconfig.sh
+++ configs/removeconfig.sh
@@ -4,4 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"/
 
 echo [Attempting to remove $1 config]
-stow -D $1 -t ~
+stow -D $1/.config/ -t ~/.config/
--- configs/setconfig.sh
+++ configs/setconfig.sh
@@ -4,4 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"/
 
 echo [Attempting to use $1 config]
-stow $1 -t ~
+stow $1/.config/ -t ~/.config/
