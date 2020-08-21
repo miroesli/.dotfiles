97a3ccdc806233d813fd2e1e8af34e3a81d85574
miroesli <misha.roesli@gmail.com>
2020-08-20 17:40:34 -0700

    remove unused config install scripts

--- configs/removeconfig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd "$parent_path"/
-
-echo [Attempting to remove $1 config]
-stow -D $1/.config/ -t ~/.config/
--- configs/setconfig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd "$parent_path"/
-
-echo [Attempting to use $1 config]
-stow $1/.config/ -t ~/.config/
