4f4b12aa537dccb1d35d6bd9a8082b1cf0ccb48a
miroesli <17695280+miroesli@users.noreply.github.com>
2020-09-24 09:39:53 -0700

    refactor: update echos and add .gitconfig

--- configs/arch/install.sh
+++ configs/arch/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 configs=(alacritty dunst i3 redshift polybar picom nvim joplin)
-dots=(.zshrc .vimrc)
+dots=(.zshrc .vimrc .gitconfig)
 
 for config in "${configs[@]}"
 do
 	cp -r .config/$config ~/.config
-	echo updated $config
 done
 for dot in "${dots[@]}"
 do
 	cp $dot ~
-	echo updated $dot
 done
+
+echo done
